7fea838cb788aac3d80e0d5759b4a619a0993dae
ShineOfMoon <kirill89535640822@gmail.com>
2026-08-26 19:47:53 +0300

    Update Paddock.py

--- Paddock.py
+++ Paddock.py
@@ -2,19 +2,19 @@
 paddocks = []
 
 class Paddock:
-    def __init__(self, *freds, *, max_freds=5):
+    def __init__(self, max_freds=5):
         paddocks.append(self)
         self.max_freds = max_freds
-        self.freddies = []
-
-        for fred in freds:
-            self.add(fred)
 
     def add(self, fred):
+    """добавляет Фреда если загон не        
+       полный, иначе возвращает False"""
+
         if Len(self.freddies) == max_freds:
-            raise Error("Загон переполнен")
-        else:
-            self.freddies.append(fred)
+            return False
+        self._freddies.append(fred)
+        return True
+
 
     def step(self):
         for fred in self.freddies:
