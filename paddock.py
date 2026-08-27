93fe8e9ae7dc95ed1acbca7570b77459efbc47de
ShineOfMoon <kirill89535640822@gmail.com>
2026-08-27 17:56:47 +0300

    Update paddock.py

--- paddock.py
+++ paddock.py
@@ -18,19 +18,19 @@ class Paddock:
         return True
 
 
-def move(self, fred, new_paddock):
-    is_added = new_paddock.add(fred)
-    if not is_added:
-        return False
+    def move(self, fred, new_paddock):
+        is_added = new_paddock.add(fred)
+        if not is_added:
+            return False
 
-    self._freddies.remove(fred)
-    fred._paddock = new_paddock
-    return True
+        self._freddies.remove(fred)
+        fred._paddock = new_paddock
+        return True
 
 
-def step(self):
-    if not self.time:
-        return
+    def step(self):
+        if not self.time:
+            return
 
-    for fred in self.freddies:
-        self.feed.step()
+        for fred in self.freddies:
+            self.feed.step()
